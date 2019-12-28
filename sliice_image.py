6185ef4bcba4b982be46e47b4b6842eb9672c911
wakamezake <hotwater1367@gmail.com>
2019-12-28 14:13:09 +0900

    upd8

--- sliice_image.py
+++ sliice_image.py
@@ -3,8 +3,11 @@ Reference:
 https://www.tensorflow.org/guide/data
 """
 
+from functools import partial
+
 import tensorflow as tf
 import tensorflow.keras as keras
+from tensorflow.keras.utils import to_categorical
 
 
 def fashion_mnist_generator():
@@ -17,11 +20,23 @@ def fashion_mnist_generator():
 
 
 def main():
+    print('tf.executing_eagerly() = ', tf.executing_eagerly())
     print(tf.__version__)
     (x_train, y_train), _ = keras.datasets.fashion_mnist.load_data()
     print(x_train.shape, y_train.shape)
     dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
     print(dataset)
+
+    num_classes = len(set(y_train))
+    to_cat = partial(tf.one_hot, depth=num_classes)
+    image_dataset = tf.data.Dataset.from_tensor_slices(x_train).map(
+        lambda x: x / 255)
+    label_dataset = tf.data.Dataset.from_tensor_slices(y_train).map(to_cat)
+    zipped_dataset = tf.data.Dataset.zip((image_dataset, label_dataset))
+    for image, label in zipped_dataset:
+        print(image)
+        print(label)
+        break
 
     batch_size = 32
     g = tf.data.Dataset.from_generator(fashion_mnist_generator,
